225a23a54db11c834a127eeb6464e755d7db526b
furPedroRibeiro <pedrocodejob@gmail.com>
2024-01-13 13:50:00 -0300

    mudando linear gradient dos cards

--- src/script.js
+++ src/script.js
@@ -5,9 +5,7 @@ fetch("https://arslibri.com.br/api/categorias").then(response => response.json()
 
 function addDataToCard(data){
     const divCards = document.getElementById('cards')
-    alert('chegou')
     for(let i = 0; i < data.length; i++){
-        alert('chegou')
         divCards.innerHTML += '<a href="" class="card" style="background-image: url(https://arslibri.com.br/'+data[i].image+');" aria-label="'+data[i].name+'"><footer class="footer-card"><p class="title">'+data[i].name+'</p></footer></a>'
     }
 }
